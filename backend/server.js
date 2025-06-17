0d5eac82703773aca5ccc355c1807f2587b448c5
jashvanth370 <jashjazz11@gmail.com>
2025-06-17 17:02:14 +0530

    Cors install

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postRoutes = require('./route/Posts')
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 8000
 //middlewere
 app.use(bodyParser.json())
+app.use(cors());
 
 //MongoDB connection
 mongoose.connect('mongodb://localhost:27017/blog')
