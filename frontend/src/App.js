1c545ea15d9ae1328200d9a19b319d4917c637e3
jashvanth370 <jashjazz11@gmail.com>
2025-06-17 18:55:41 +0530

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import Footers from './component/Footers';
+import Footer from './component/Footers';
+import Headers from './component/Headers';
+import PostDetails from './pages/PostDetails';
 import PostList from './pages/PostList';
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <Headers />
+      <Router>
+        <Routes>
+          <Route path='/' element={<PostList />} />
+          <Route path='/posts/details/:id' element={<PostDetails />} />
+        </Routes>
+      </Router>
+      <Footers />
     </div>
   );
 }
